2660269513ae87e5deb3f11bc637f96a3a23a3f4
Vihaan <vihaanverma21@gmail.com>
2017-12-20 19:05:09 +0530

    first test

--- app/src/androidTest/java/com/vihaan/basicsample/ChangeTextBehaviorTest.java
+++ app/src/androidTest/java/com/vihaan/basicsample/ChangeTextBehaviorTest.java
@@ -1,5 +1,8 @@
 package com.vihaan.basicsample;
 
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.action.TypeTextAction;
+import android.support.test.espresso.proto.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -7,6 +10,16 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.StringTokenizer;
+
+import static android.support.test.espresso.Espresso.closeSoftKeyboard;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 /**
  * Created by vihaanverma on 20/12/17.
@@ -25,6 +38,11 @@ public class ChangeTextBehaviorTest {
     @Test
 
     public void changeText_sameActivity() {
+        onView(withId(R.id.editTextUserInput))
+                .perform(typeText(STRING_TO_BE_TYPED));
+        closeSoftKeyboard();
+        onView(withId(R.id.changeTextBt)).perform(click());
+        onView(withId(R.id.textToBeChanged)).check(matches(withText(STRING_TO_BE_TYPED)));
 
     }
 
